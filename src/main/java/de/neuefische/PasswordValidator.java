4aa219a8576bc22b1c25126ae1b6e0054a084bf8
Gula <gulya@gmx.net>
2024-10-01 16:03:02 +0200

    feat: isPasswordLongEnough

---
+++ src/main/java/de/neuefische/PasswordValidator.java
@@ -0,0 +1,10 @@
+package de.neuefische;
+
+public class PasswordValidator {
+    public static boolean isPasswordLongEnough (char[] password) {
+return password.length>=8;
+    }
+//    public static boolean passwordHasDigits (char[] password)
+//    public static boolean isPasswordWithUpperAndLowerCase (char[] password)
+//    public static boolean isPasswordInCommonUse (char[] password)
+}
