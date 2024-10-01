f86d0ad4976dcb1191aa5d067b3da988a4b6b64a
Gula <gulya@gmx.net>
2024-10-01 16:25:39 +0200

    feat: passwordHasDigits

--- src/main/java/de/neuefische/PasswordValidator.java
+++ src/main/java/de/neuefische/PasswordValidator.java
@@ -1,10 +1,19 @@
 package de.neuefische;
 
 public class PasswordValidator {
-    public static boolean isPasswordLongEnough (char[] password) {
-return password.length>=8;
+    public static boolean isPasswordLongEnough(char[] password) {
+        return password.length >= 8;
     }
-//    public static boolean passwordHasDigits (char[] password)
+
+    public static boolean passwordHasDigits(char[] password) {
+        for (char symbol : password) {
+            if (Character.isDigit(symbol)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
 //    public static boolean isPasswordWithUpperAndLowerCase (char[] password)
 //    public static boolean isPasswordInCommonUse (char[] password)
 }
